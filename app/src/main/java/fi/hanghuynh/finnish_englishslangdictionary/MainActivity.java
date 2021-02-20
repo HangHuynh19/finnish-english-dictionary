8bd60972a22e052fbb7dbece584f65a621a7b52d
David Fallow <David.Fallow@metropolia.fi>
2021-02-20 11:16:53 +0200

    Template UserStatistics page with back btn

--- app/src/main/java/fi/hanghuynh/finnish_englishslangdictionary/MainActivity.java
+++ app/src/main/java/fi/hanghuynh/finnish_englishslangdictionary/MainActivity.java
@@ -1,7 +1,7 @@
 package fi.hanghuynh.finnish_englishslangdictionary;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("button", "start button");
         } else if (v.getId() == R.id.statisticsBtn){
             Log.d("button", "statistics button");
+            startActivity(new Intent(MainActivity.this, UserStatistics.class));
         }
     };
 
@@ -27,5 +28,26 @@ public class MainActivity extends AppCompatActivity {
 
         btnStart.setOnClickListener(onClickListener);
         btnStats.setOnClickListener(onClickListener);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("method", "onStart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("method", "onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("method", "onPause");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("method", "onDestroy");
     }
 }
