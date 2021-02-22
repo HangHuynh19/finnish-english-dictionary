5c8be47b2d62dfbb9bd295b3d963ad10ad9be188
Hang Huynh <Thanh.Huynh2@metropolia.fi>
2021-02-22 16:23:22 +0200

    build Word class, Word DAO using Room, populate database

--- app/src/main/java/fi/hanghuynh/finnish_englishslangdictionary/MainActivity.java
+++ app/src/main/java/fi/hanghuynh/finnish_englishslangdictionary/MainActivity.java
@@ -3,12 +3,37 @@ package fi.hanghuynh.finnish_englishslangdictionary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+import fi.hanghuynh.finnish_englishslangdictionary.db.AppDatabase;
+import fi.hanghuynh.finnish_englishslangdictionary.db.Word;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "DICTIONARY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //AppDatabase.getDbInstance(this.getApplicationContext()).wordDAO().deleteAll();
+        saveWordToDictionary();
+        List<Word> wordList = AppDatabase.getDbInstance(this.getApplicationContext()).wordDAO().getAllWords();
+
+    }
+
+    private void saveWordToDictionary() {
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+
+        if (db.wordDAO().getAnyWord().length < 1) {
+            for (int i = 0; i < db.getWordArraySize(); i++) {
+                db.wordDAO().insertWord(db.getWord(i));
+            }
+
+            finish();
+        }
     }
 }
